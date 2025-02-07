faee118fefc089ba18f3d3d069015a735fc116e5
Harry.kouevi <harry.kouevi@gmaol.com>
2025-02-07 20:02:15 +0000

    last of 07-02-2025

--- lib/common/widgets/custom_shapes/containers/primary_hearder_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_hearder_container.dart
@@ -20,7 +20,7 @@ class TPrimaryHeaderContainer extends StatelessWidget {
     return TCurvedEdgesWidget(
       // Ce widget enveloppe le contenu pour ajouter des bords incurvés.
       child: Container(
-        color: TColors.blue, // Définit la couleur d'arrière-plan principale.
+        //color: TColors.blue, // Définit la couleur d'arrière-plan principale.
         padding: const EdgeInsets.all(0), // Aucun espacement interne.
         child: SizedBox(
           height: 400, // Définit la hauteur fixe du conteneur.
@@ -29,13 +29,16 @@ class TPrimaryHeaderContainer extends StatelessWidget {
             children: [
               /// Cercle décoratif situé en haut à droite, en dehors des limites visibles.
               Positioned(
-                top: -150, // Positionne le cercle au-dessus de la limite visible.
-                right: -250, // Positionne le cercle à droite en dehors de l'écran.
+                top:
+                    -150, // Positionne le cercle au-dessus de la limite visible.
+                right:
+                    -250, // Positionne le cercle à droite en dehors de l'écran.
                 child: TCircularContainer(
                   // Conteneur circulaire avec une opacité réduite.
                   backgroundColor: TColors.textWhite.withOpacity(0.1),
                 ),
               ),
+
               /// Cercle décoratif situé légèrement plus bas à droite.
               Positioned(
                 top: 100, // Positionne le cercle plus bas que le premier.
@@ -44,6 +47,7 @@ class TPrimaryHeaderContainer extends StatelessWidget {
                   backgroundColor: TColors.textWhite.withOpacity(0.1),
                 ),
               ),
+
               /// Contenu principal du conteneur.
               child,
             ],
--- lib/features/shop/screens/home/widgets/promo_slider.dart
+++ lib/features/shop/screens/home/widgets/promo_slider.dart
@@ -21,22 +21,24 @@ class TPromoSlider extends StatelessWidget {
     final controller = Get.put(HomeController());
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
-    final double sliderHeight = screenHeight * 0.2; // 20% de la hauteur
+    final double sliderHeight = screenHeight * 0.4; // 20% de la hauteur
 
     return Column(
       children: [
         CarouselSlider(
-          items: banners.map((url) =>
-              SizedBox(
-                width: screenWidth, // 100% de la largeur
-                height: sliderHeight, // 20% de la hauteur
-                child: TRoundedImage(
-                  imageUrl: url,
-                  applyImageRadius: false, // S'assurer que l'image prend tout l'espace
-                  fit: BoxFit.cover, // Ajustement pour occuper tout l'espace
-                ),
-              )
-          ).toList(),
+          items: banners
+              .map((url) => SizedBox(
+                    width: screenWidth, // 100% de la largeur
+                    height: sliderHeight, // 20% de la hauteur
+                    child: TRoundedImage(
+                      imageUrl: url,
+                      applyImageRadius:
+                          false, // S'assurer que l'image prend tout l'espace
+                      fit:
+                          BoxFit.cover, // Ajustement pour occuper tout l'espace
+                    ),
+                  ))
+              .toList(),
           options: CarouselOptions(
             height: sliderHeight,
             viewportFraction: 1,
@@ -52,7 +54,7 @@ class TPromoSlider extends StatelessWidget {
         const SizedBox(height: TSizes.spacetBtwItems),
         Center(
           child: Obx(
-                () => Row(
+            () => Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 for (int i = 0; i < banners.length; i++)
