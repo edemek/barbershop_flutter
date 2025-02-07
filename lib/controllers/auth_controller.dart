c076792839b9f6618e384dfa895f0eeb7313e660
Harry.kouevi <harry.kouevi@gmaol.com>
2025-02-07 18:56:43 +0000

    jjjjjj

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,13 +2,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-import 'package:barbershpo_flutter/data/repositories/user.dart';
 import 'package:barbershpo_flutter/data/services/api_service.dart';
 import 'package:barbershpo_flutter/features/personalization/screens/profile/profile.dart';
 import '../features/services/auth_service.dart';
 
 import '../../../../common/ui.dart';
-// import '../../../models/user_model.dart';
+import '../../../models/user_model.dart';
 import '../../features/repositories/user_repository.dart';
 import '../../../routes/app_routes.dart';
 import '../../../features/services/auth_service.dart';
@@ -25,62 +24,54 @@ class AuthController extends GetxController {
   // Stocke l'utilisateur actuel sous forme de variable réactive.
   final Rx<User> currentUser = Get.find<AuthService>().user;
   // final Rx<User?> currentUser = Rx<User?>(null);
-  late GlobalKey<FormState> loginFormKey;
+  // late GlobalKey<FormState> loginFormKey;
+  final loginFormKey = GlobalKey<FormState>();
 
   static AuthController get to => Get.find(); // Access the
 
   // Indique si une opération est en cours (chargement).
-  final RxBool isLoading = false.obs;
-  final loading = false.obs;
+  final RxBool loading = false.obs;
+  // final loading = false.obs;
 
   // Contient les messages d'erreur pour les afficher à l'utilisateur.
   final RxString error = "".obs;
 
   // Indique si l'utilisateur est connecté ou non.
-  bool get isLoggedIn => currentUser.value != null;
+  // bool get isLoggedIn => currentUser.value != null;
 
   // Méthode pour enregistrer un nouvel utilisateur.
   Future<void> register(String name, String email, String password,
       String passwordConfirmation) async {
+    loading.value = true; // Début du chargement.
     try {
-      isLoading.value = true; // Début du chargement.
-      error.value = ""; // Réinitialisation des erreurs.
+      // await _userRepository.sendCodeToPhone();
+      loading.value = false;
+      await Get.toNamed(Routes.PHONE_VERIFICATION);
 
-      // Appel de l'API pour enregistrer l'utilisateur.
-      final newUser = await _apiService.register(
-          name, email, password, passwordConfirmation);
-
-      // Mise à jour de l'utilisateur actuel.
-      currentUser.value = newUser;
-
-      // Récupération des tâches de l'utilisateur.
-      //await Get.find<TodoController>().fetchTodos();
-
-      // Navigation vers la page des tâches.
-      //Get.offAllNamed('/todos');
       Get.offAll(ProfileScreen());
     } catch (e) {
       // Capture et affichage des erreurs.
       error.value = e.toString();
     } finally {
-      // Fin du chargement.
-      isLoading.value = false;
+      loading.value = false;
     }
   }
 
   // Méthode pour connecter un utilisateur.
-  Future<void> login(String email, String password) async {
+  Future<void> login(String phonenumber, String password) async {
     print('nous etions dans le controleur');
     //------------------------------
+    print(loginFormKey.currentState);
+    print(loginFormKey.currentState!.validate());
     Get.focusScope?.unfocus();
     if (loginFormKey.currentState!.validate()) {
       loginFormKey.currentState!.save();
       loading.value = true;
       try {
+        print(currentUser.value);
         // await Get.find<FireBaseMessagingService>().setDeviceToken();
         currentUser.value = await _userRepository.login(currentUser.value);
-        await _userRepository.signInWithEmailAndPassword(
-            currentUser.value.email!, currentUser.value.apiToken!);
+        //await _userRepository.signInWithEmailAndPassword(currentUser.value.email!, currentUser.value.apiToken!);
         await Get.find<RootController>().changePage(0);
       } catch (e) {
         Get.showSnackbar(Ui.ErrorSnackBar(message: e.toString()));
@@ -89,31 +80,31 @@ class AuthController extends GetxController {
       }
     }
 
-    //''''''''''''''''''''''''''''''''
+    // //''''''''''''''''''''''''''''''''
 
-    try {
-      isLoading.value = true; // Début du chargement.
-      error.value = ""; // Réinitialisation des erreurs.
+    // try {
+    //   isLoading.value = true; // Début du chargement.
+    //   error.value = ""; // Réinitialisation des erreurs.
 
-      // Appel de l'API pour authentifier l'utilisateur.
-      final user = await _apiService.login(email, password);
+    //   // Appel de l'API pour authentifier l'utilisateur.
+    //   final user = await _apiService.login(email, password);
 
-      // Mise à jour de l'utilisateur actuel.
-      currentUser.value = user;
+    //   // Mise à jour de l'utilisateur actuel.
+    //   currentUser.value = user;
 
-      // Récupération des tâches de l'utilisateur.
-      //await Get.find<TodoController>().fetchTodos();
+    //   // Récupération des tâches de l'utilisateur.
+    //   //await Get.find<TodoController>().fetchTodos();
 
-      // Navigation vers la page des tâches.
-      //Get.offAllNamed('/todos');
-      Get.offAll(ProfileScreen());
-    } catch (e) {
-      // Capture et affichage des erreurs.
-      error.value = e.toString();
-    } finally {
-      // Fin du chargement.
-      isLoading.value = false;
-    }
+    //   // Navigation vers la page des tâches.
+    //   //Get.offAllNamed('/todos');
+    //   Get.offAll(ProfileScreen());
+    // } catch (e) {
+    //   // Capture et affichage des erreurs.
+    //   error.value = e.toString();
+    // } finally {
+    //   // Fin du chargement.
+    //   isLoading.value = false;
+    // }
   }
 
 /*
--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -89,9 +89,9 @@ class _TLoginFormState extends State<TLoginForm> {
   @override
   Widget build(BuildContext context) {
     // controller.loginFormKey = new GlobalKey<FormState>();
-    final userController = Get.find<UserController>();
+    final controller = Get.find<AuthController>();
     return Form(
-      key: _formKey,
+      key: controller.loginFormKey,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: TSizes.spacetBtwSections),
         child: Column(
@@ -243,9 +243,10 @@ class _TLoginFormState extends State<TLoginForm> {
               child: ElevatedButton(
                 child: const Text(TTexts.signIn),
                 onPressed: () {
-                  AuthController.to
-                      .login(_PhoneController.text, _passwordController.text);
-                  /*if (_formKey.currentState!.validate()) {
+                  controller.login(
+                      _PhoneController.text, _passwordController.text);
+                  /*
+                  if (_formKey.currentState!.validate()) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text('OTP Envoié !'),
@@ -347,191 +348,3 @@ class _TLoginFormState extends State<TLoginForm> {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:http/http.dart' as http;
-import '../../../../../utils/constants/sizes.dart';
-import '../../../../../utils/constants/text_strings.dart';
-import '../../../../../utils/validators/validation.dart';
-import '../../../../personalization/screens/profile/profile.dart';
-import '../../signup/signup.dart';
-import '../../signup/verify_email.dart';
-import '../../signup/widgets/signup_form.dart';
-
-class TLoginForm extends StatefulWidget {
-  TLoginForm({super.key});
-
-  @override
-  _TLoginFormState createState() => _TLoginFormState();
-}
-
-class _TLoginFormState extends State<TLoginForm> {
-  // Clé globale pour accéder au formulaire
-  final _formKey = GlobalKey<FormState>();
-
-  // Champs de saisie
-  final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  bool _obscureText = true; // Contrôle l'état du texte masqué
-
-  void sendOtp(BuildContext context) async {
-    final response = await http.post(
-      Uri.parse('http://votre-backend-url/api/send-otp'),
-      body: {'phone_number': _phoneController.text},
-    );
-
-    if (response.statusCode == 200) {
-      // Aller à l'écran de saisie de l'OTP
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) =>
-              VerifyEmailScreen(emailOrPhoneNumber: _phoneController.text),
-        ),
-      );
-    } else {
-      // Afficher une erreur
-      print('Erreur : ${response.body}');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: TSizes.spacetBtwSections),
-        child: Column(
-          children: [
-            /// -- Numéro de Téléphone
-            TextFormField(
-              controller: _phoneController,
-              keyboardType: TextInputType.phone,
-              inputFormatters: [
-                TogoleseemailOrPhoneNumberFormatter(), // Formatter personnalisé
-              ],
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.direct_right),
-                labelText: "Numéro de téléphone",
-                hintText: "+228 90 90 90 90",
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Veuillez entrer votre numéro de téléphone';
-                }
-
-                // Supprime les espaces pour la validation
-                final cleanedValue =
-                value.replaceAll(' ', '').replaceAll('+228', '');
-
-                // Validation pour s'assurer qu'il contient exactement 8 chiffres
-                if (cleanedValue.length != 8 ||
-                    !RegExp(r'^\d{8}$').hasMatch(cleanedValue)) {
-                  return 'Numéro de téléphone invalide';
-                }
-
-                return null;
-              },
-            ),
-
-            const SizedBox(height: TSizes.spaceBtwInputFields),
-
-            /// -- Mot de passe
-            TextFormField(
-              controller: _passwordController,
-              obscureText: _obscureText,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Iconsax.password_check),
-                labelText: TTexts.password,
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Iconsax.eye_slash : Iconsax.eye,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText; // Bascule entre masqué et visible
-                    });
-                  },
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Veuillez entrer un mot de passe';
-                } else if (value.length < 6) {
-                  return 'Le mot de passe doit contenir au moins 6 caractères';
-                } else if (!RegExp(r'[A-Z]').hasMatch(value)) {
-                  return 'Le mot de passe doit contenir au moins une lettre majuscule';
-                } else if (!RegExp(r'[0-9]').hasMatch(value)) {
-                  return 'Le mot de passe doit contenir au moins un chiffre';
-                } else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-                  return 'Le mot de passe doit contenir au moins un caractère spécial';
-                }
-                return null;
-              },
-            ),
-
-            const SizedBox(height: TSizes.spaceBtwInputFields / 2),
-
-            /// -- Remember Me & Forget Password
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                /// Remember Me
-                Row(
-                  children: [
-                    Checkbox(value: true, onChanged: (value) {}),
-                    const Text(TTexts.remenderMe),
-                  ],
-                ),
-
-                /// Forget Password
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(TTexts.forgotPassword),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: TSizes.spacetBtwSections),
-
-            /// -- Sign In Button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Si la validation réussit
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Connexion faite avec succès !')),
-                    );
-                    sendOtp(context);
-                    Get.to(() => ProfileScreen());
-                  }
-                },
-                child: const Text(TTexts.signIn),
-              ),
-            ),
-            const SizedBox(height: TSizes.spacetBtwItems),
-
-            /// -- Create Account Button
-            SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () => Get.to(() => const SignupScreen()),
-                child: const Text(TTexts.createAccount),
-              ),
-            ),
-
-            const SizedBox(height: TSizes.spacetBtwItems),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-*/
